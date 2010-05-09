37e0db53e2cc9d0c1ef43cd72bbdb0e7f75ddefa
petero <petero@738e98cc-9000-e54f-6aee-11b01496cf95>
2010-05-09 20:17:28 +0000

    Draw an ascii chess board. No other chess functionality in the program though.

--- CuckooChessAPK/src/org/petero/cuckoochess/CuckooChess.java
+++ CuckooChessAPK/src/org/petero/cuckoochess/CuckooChess.java
@@ -1,7 +1,11 @@
 package org.petero.cuckoochess;
 
+import chess.ChessParseError;
+import chess.Position;
+import chess.TextIO;
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CuckooChess extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,13 @@ public class CuckooChess extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        try {
+			Position pos = TextIO.readFEN(TextIO.startPosFEN);
+			String s = TextIO.asciiBoard(pos);
+			TextView tv = (TextView)this.findViewById(R.id.textview);
+			tv.setText(s);
+		} catch (ChessParseError e) {
+			e.printStackTrace();
+		}
     }
 }
