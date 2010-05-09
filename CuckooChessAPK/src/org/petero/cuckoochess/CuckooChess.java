121d704ea315a9e3588dc20d8da7b5fc298698e7
petero <petero@738e98cc-9000-e54f-6aee-11b01496cf95>
2010-05-09 19:31:09 +0000

    Initial import. Just the eclipse default, there is no chess code here yet.

---
+++ CuckooChessAPK/src/org/petero/cuckoochess/CuckooChess.java
@@ -0,0 +1,13 @@
+package org.petero.cuckoochess;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CuckooChess extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
