d3283b775f2207cfffd3d1d1ed55d804b6514b0e
petero <petero@738e98cc-9000-e54f-6aee-11b01496cf95>
2010-05-15 10:43:51 +0000

    Created a GUIInterface that will be used by both the Java SE gui and the Android gui.

---
+++ CuckooChessEngine/src/guibase/GUIInterface.java
@@ -0,0 +1,30 @@
+package guibase;
+
+import chess.Position;
+
+public interface GUIInterface {
+
+	/** Update the displayed board position. */
+	public void setPosition(Position pos);
+
+	/** Mark square i as selected */
+	public void setSelection(int sq);
+
+	/** Set the status text. */
+	public void setStatusString(String str);
+
+	/** Update the list of moves. */
+	public void setMoveListString(String str);
+
+	/** Get the current time limit. */
+	public int timeLimit();
+
+	/** Return true if "show thinking" is enabled. */
+	public boolean showThinking();
+
+	/** Ask for which piece to promote a pawn to. Should be implemented as a modal dialog. */
+	public int getPromotePiece();
+
+	/** Run code on the GUI thread. */
+	public void runOnUIThread(Runnable runnable);
+}
