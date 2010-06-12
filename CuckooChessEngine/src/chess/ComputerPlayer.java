bfcaabdf4142bf47315d7e5e0fce293c5ccdb975
petero <petero@738e98cc-9000-e54f-6aee-11b01496cf95>
2010-06-12 16:01:54 +0000

    Changed version number to 1.05a1.

--- CuckooChessEngine/src/chess/ComputerPlayer.java
+++ CuckooChessEngine/src/chess/ComputerPlayer.java
@@ -13,7 +13,7 @@ import java.util.List;
  * @author petero
  */
 public class ComputerPlayer implements Player {
-    public static String engineName = "CuckooChess 1.04";
+    public static String engineName = "CuckooChess 1.05a1";
 
     int minTimeMillis;
     int maxTimeMillis;
