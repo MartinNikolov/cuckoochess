5f7528858b584e88edb6c8dc18a358fcd84e5142
petero <petero@738e98cc-9000-e54f-6aee-11b01496cf95>
2010-07-02 07:24:10 +0000

    First version of the binary book support. It currently has at least two limitations: 1. You have to manually rebuild the book.bin file using the Book.main() function. 2. The book.txt file is included in jar/apk files, even though it isn't used.

--- CuckooChessEngine/src/chess/Book.java
+++ CuckooChessEngine/src/chess/Book.java
@@ -6,6 +6,7 @@
 package chess;
 
 import java.io.BufferedReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -22,7 +23,6 @@ import java.util.Random;
  * @author petero
  */
 public class Book {
-	// FIXME!!! Preprocess book to binary format.
     public static class BookEntry {
         Move move;
         int count;
@@ -47,22 +47,32 @@ public class Book {
         rndGen = new SecureRandom();
         rndGen.setSeed(System.currentTimeMillis());
         numBookMoves = 0;
-        try {
-            InputStream inStream = getClass().getResourceAsStream("/book.txt");
-            InputStreamReader inFile = new InputStreamReader(inStream);
-            BufferedReader inBuf = new BufferedReader(inFile);
-            LineNumberReader lnr = new LineNumberReader(inBuf);
-            String line;
-            while ((line = lnr.readLine()) != null) {
-                if (line.startsWith("#") || (line.length() == 0)) {
-                    continue;
-                }
-                if (!addBookLine(line)) {
-                    System.out.printf("Book parse error, line:%d\n", lnr.getLineNumber());
-                    throw new RuntimeException();
-                }
-//                System.out.printf("no:%d line:%s%n", lnr.getLineNumber(), line);
+    	try {
+            InputStream inStream = getClass().getResourceAsStream("/book.bin");
+            List<Byte> buf = new ArrayList<Byte>(8192);
+            byte[] tmpBuf = new byte[1024];
+            while (true) {
+            	int len = inStream.read(tmpBuf);
+            	if (len <= 0) break;
+            	for (int i = 0; i < len; i++)
+            		buf.add(tmpBuf[i]);
             }
+            Position startPos = TextIO.readFEN(TextIO.startPosFEN);
+	        Position pos = new Position(startPos);
+	        UndoInfo ui = new UndoInfo();
+	        int len = buf.size();
+	        for (int i = 0; i < len; i += 2) {
+	        	int b0 = buf.get(i); if (b0 < 0) b0 += 256;
+	        	int b1 = buf.get(i+1); if (b1 < 0) b1 += 256;
+	        	int move = (b0 << 8) + b1;
+	        	if (move == 0) {
+	        		pos = new Position(startPos);
+	        	} else {
+	        		Move m = new Move(move & 63, (move >> 6) & 63, (move >> 12) & 15);
+	        		addToBook(pos, m);
+	        		pos.makeMove(m, ui);
+	        	}
+	        }
         } catch (ChessParseError ex) {
             throw new RuntimeException();
         } catch (IOException ex) {
@@ -75,7 +85,26 @@ public class Book {
                     (t1 - t0) / 1000.0);
         }
     }
-    
+
+    /** Add a move to a position in the opening book. */
+    private final void addToBook(Position pos, Move moveToAdd) {
+        List<BookEntry> ent = bookMap.get(pos.zobristHash());
+        if (ent == null) {
+            ent = new ArrayList<BookEntry>();
+            bookMap.put(pos.zobristHash(), ent);
+        }
+        for (int i = 0; i < ent.size(); i++) {
+            BookEntry be = ent.get(i);
+            if (be.move.equals(moveToAdd)) {
+                be.count++;
+                return;
+            }
+        }
+        BookEntry be = new BookEntry(moveToAdd);
+        ent.add(be);
+        numBookMoves++;
+    }
+
     /** Return a random book move for a position, or null if out of book. */
     public final Move getBookMove(Position pos) {
         List<BookEntry> bookMoves = bookMap.get(pos.zobristHash());
@@ -129,8 +158,47 @@ public class Book {
         return ret.toString();
     }
 
-    /** Add a sequence of moves, starting from pos, to the opening book. */
-    private final boolean addBookLine(String line) throws ChessParseError {
+    /** Creates the book.bin file. */
+    public static void main(String[] args) throws IOException {
+    	List<Byte> binBook = createBinBook();
+    	FileOutputStream out = new FileOutputStream("src/book.bin");
+    	int bookLen = binBook.size();
+    	byte[] binBookA = new byte[bookLen];
+    	for (int i = 0; i < bookLen; i++)
+    		binBookA[i] = binBook.get(i);
+    	out.write(binBookA);
+    	out.close();
+    }
+    
+    public static List<Byte> createBinBook() {
+    	List<Byte> binBook = new ArrayList<Byte>(0);
+        try {
+            InputStream inStream = new Object().getClass().getResourceAsStream("/book.txt");
+            InputStreamReader inFile = new InputStreamReader(inStream);
+            BufferedReader inBuf = new BufferedReader(inFile);
+            LineNumberReader lnr = new LineNumberReader(inBuf);
+            String line;
+            while ((line = lnr.readLine()) != null) {
+                if (line.startsWith("#") || (line.length() == 0)) {
+                    continue;
+                }
+                if (!addBookLine(line, binBook)) {
+                    System.out.printf("Book parse error, line:%d\n", lnr.getLineNumber());
+                    throw new RuntimeException();
+                }
+//              System.out.printf("no:%d line:%s%n", lnr.getLineNumber(), line);
+            }
+        } catch (ChessParseError ex) {
+            throw new RuntimeException();
+        } catch (IOException ex) {
+            System.out.println("Can't read opening book resource");
+            throw new RuntimeException();
+        }
+    	return binBook;
+    }
+
+    /** Add a sequence of moves, starting from the initial position, to the binary opening book. */
+    private static boolean addBookLine(String line, List<Byte> binBook) throws ChessParseError {
         Position pos = TextIO.readFEN(TextIO.startPosFEN);
         UndoInfo ui = new UndoInfo();
         String[] strMoves = line.split(" ");
@@ -140,28 +208,13 @@ public class Book {
             if (m == null) {
                 return false;
             }
-            addToBook(pos, m);
+            int val = m.from + (m.to << 6) + (m.promoteTo << 12);
+            binBook.add((byte)(val >> 8));
+            binBook.add((byte)(val & 255));
             pos.makeMove(m, ui);
         }
+        binBook.add((byte)0);
+        binBook.add((byte)0);
         return true;
-    }
-    
-    /** Add a move to a position in the opening book. */
-    private final void addToBook(Position pos, Move moveToAdd) {
-        List<BookEntry> ent = bookMap.get(pos.zobristHash());
-        if (ent == null) {
-            ent= new ArrayList<BookEntry>();
-            bookMap.put(pos.zobristHash(), ent);
-        }
-        for (int i = 0; i < ent.size(); i++) {
-            BookEntry be = ent.get(i);
-            if (be.move.equals(moveToAdd)) {
-                be.count++;
-                return;
-            }
-        }
-        BookEntry be = new BookEntry(moveToAdd);
-        ent.add(be);
-        numBookMoves++;
     }
 }
