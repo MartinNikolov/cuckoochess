4a0294261649b1f243e8195811b44981a8d45cca
petero <petero@738e98cc-9000-e54f-6aee-11b01496cf95>
2010-07-17 21:06:06 +0000

    DroidFish: Removed bogus filename for stlport build.

--- DroidFish/jni/stlport/src/Android.mk
+++ DroidFish/jni/stlport/src/Android.mk
@@ -7,7 +7,7 @@ LOCAL_MODULE := stlport
 LOCAL_SRC_FILES := \
 	strstream.cpp messages.cpp iostream.cpp ctype.cpp dll_main.cpp complex_trig.cpp \
 	locale_catalog.cpp complex_io.cpp allocators.cpp string.cpp ostream.cpp \
-	locale_impl.cpp libstlport.so locale.cpp istream.cpp time_facets.cpp fstream.cpp \
+	locale_impl.cpp locale.cpp istream.cpp time_facets.cpp fstream.cpp \
 	bitset.cpp codecvt.cpp stdio_streambuf.cpp complex.cpp ios.cpp sstream.cpp \
 	num_put.cpp num_put_float.cpp cxa.c facets_byname.cpp numpunct.cpp monetary.cpp \
 	num_get_float.cpp num_get.cpp c_locale.c collate.cpp 
